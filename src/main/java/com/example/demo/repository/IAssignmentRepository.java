e81a8b4b7f011d4447f76f3d8b0102c8c179c2a2
JhoanTovar <tovarjhoan82sf@gmail.com>
2026-09-24 16:03:47 -0500

    commit hora 2 final

--- src/main/java/com/example/demo/repository/IAssignmentRepository.java
+++ src/main/java/com/example/demo/repository/IAssignmentRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface IAssignmentRepository extends JpaRepository<Assignment, Long> {
 
-    List<Assignment> findByClassroom_TeacherNameEqualsAndRepositories_Pullrequests_Reviewer_NameEqualsAndRepositories_Pullrequests_Status(String classroom_TeacherName, String repositories_Pullrequests_Reviewer_Name, String status);
+    List<Assignment> findByClassroom_Teacher_NameAndRepositories_PullRequests_Reviewer_NameEqualsAndRepositories_PullRequests_Status(String classroom_TeacherName, String repositories_PullRequests_Reviewer_Name, String status);
 
 }
